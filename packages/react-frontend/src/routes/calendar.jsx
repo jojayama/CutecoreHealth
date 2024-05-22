028c20e1aedcd2c684830eef2e252ae22db22207
Charlotte Maples <cgmaples13@gmail.com>
2024-05-21 21:17:37 -0700

    added a year view and attemped to add a font (not successful)

--- packages/react-frontend/src/routes/calendar.jsx
+++ packages/react-frontend/src/routes/calendar.jsx
@@ -3,15 +3,16 @@ import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import listPlugin from '@fullcalendar/list';
+import multiMonthPlugin from '@fullcalendar/multimonth'
 
 export default function CuteCalendar() {
   return (
     <FullCalendar
-      plugins = {[ dayGridPlugin, timeGridPlugin, listPlugin ]}
+      plugins = {[ dayGridPlugin, timeGridPlugin, listPlugin, multiMonthPlugin ]}
       headerToolbar={{
         left: 'prev,next today',
         center: 'title',
-        right: 'dayGridMonth,timeGridWeek,timeGridDay'
+        right: 'dayGridMonth,timeGridWeek,timeGridDay,multiMonthYear'
       }}
       initialView="dayGridMonth"
       selectable={true}
