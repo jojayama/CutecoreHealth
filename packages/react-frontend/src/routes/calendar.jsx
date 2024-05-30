7cee82d7a042f6343de3e74112b1367df3be7e34
Charlotte Maples <cgmaples13@gmail.com>
2024-05-29 23:51:33 -0700

    worked on events

--- packages/react-frontend/src/routes/calendar.jsx
+++ packages/react-frontend/src/routes/calendar.jsx
@@ -3,11 +3,49 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import listPlugin from "@fullcalendar/list";
 import multiMonthPlugin from "@fullcalendar/multimonth";
+import interactionPlugin from '@fullcalendar/interaction';
 import styles from "../style/CuteCalendar.module.css";
 import Navbar from "../navbar";
 import Layout from "./layout";
+import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+
+const EventItem = ({ info }) => {
+  const { event } = info;
+  return (
+    <div>
+      <p>{event.title}</p>
+    </div>
+  );
+};
 
 export default function CuteCalendar() {
+  const [events, setEvents] = useState([]);
+
+  const handleSelect = (info) => {
+    const { start, end } = info;
+    const eventNamePrompt = prompt("Enter event name");
+    if (eventNamePrompt) {
+      setEvents([
+        ...events,
+        {
+          start,
+          end,
+          title: eventNamePrompt,
+          id: uuidv4()
+        }
+      ]);
+    }
+  };
+
+  const selectEvent = (info) => {
+
+  };
+
+  const feelingsTracker = (info) => {
+
+  };
+
   return (
     <div>
       <link
@@ -23,13 +61,25 @@ export default function CuteCalendar() {
             timeGridPlugin,
             listPlugin,
             multiMonthPlugin,
+            interactionPlugin
           ]}
           headerToolbar={{
             left: "prev,next today",
             center: "title",
             right: "dayGridMonth,timeGridWeek,timeGridDay,multiMonthYear",
           }}
+          events={events}
+          select={handleSelect}
+          // eventClick={}
+          customButtons={{
+            feelingsTracker: {
+              text: 'feeling',
+              click: feelingsTracker()
+            }
+          }}
+          eventContent={(info) => <EventItem info={info}/>}
           initialView="dayGridMonth"
+          nowIndicator={true}
           selectable={true}
         />
       </div>
