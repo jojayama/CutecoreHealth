6a5cbe098bfe285c23d2c54361dbf3e2ded328bd
Charlotte Maples <cgmaples13@gmail.com>
2024-06-04 22:17:37 -0700

    removed confirmation that the backend connected to MongoDB (now it just throws an error if it doesn't work), and removed errors related to running tests

--- packages/express-backend/backend.test.js
+++ packages/express-backend/backend.test.js
@@ -1,47 +1,49 @@
 //backend.test.js
+import mongoose from "mongoose";
 import mut from "./user-services";
 
 test("Testing findUserByName() -- success", () => {
-    const expected = "";
-    const got = "";
-    expect(got).toBe(expected);
+  const expected = "";
+  const got = "";
+  expect(got).toBe(expected);
 });
 
 test("Testing findUserById() -- success", () => {
-    const expected = "";
-    const got = "";
-    expect(got).toBe(expected);
+  const expected = "";
+  const got = "";
+  expect(got).toBe(expected);
 });
 
 test("Testing addUser() -- success", () => {
-    const expected = "";
-    const got = "";
-    expect(got).toBe(expected);
+  const expected = "";
+  const got = "";
+  expect(got).toBe(expected);
 });
 
 test("Testing findUserByEmail() -- success", () => {
-    const expected = "";
-    const got = "";
-    expect(got).toBe(expected);
+  const expected = "";
+  const got = "";
+  expect(got).toBe(expected);
 });
 
-test(
-    "Testing findUserByNameAndEmail() -- success",
-    () => {
-        const expected = "";
-        const got = "";
-        expect(got).toBe(expected);
-    },
-    );
-    
-test("Testing getUsers() -- success", () => {
-    const expected = "";
-    const got = "";
-    expect(got).toBe(expected);
+test("Testing findUserByNameAndEmail() -- success", () => {
+  const expected = "";
+  const got = "";
+  expect(got).toBe(expected);
 });
-    
+
+test("Testing getUsers() -- success", () => {
+  const expected = "";
+  const got = "";
+  expect(got).toBe(expected);
+});
+
 test("Testing deleteUserById() -- success", () => {
-    const expected = "";
-    const got = "";
-    expect(got).toBe(expected);
+  const expected = "";
+  const got = "";
+  expect(got).toBe(expected);
+});
+
+afterAll(() => {
+  mongoose.connection.close();
 });
--- packages/express-backend/user-services.js
+++ packages/express-backend/user-services.js
@@ -14,9 +14,6 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
   .catch((error) => {
     console.error("Error connecting to MongoDB:", error);
   });
