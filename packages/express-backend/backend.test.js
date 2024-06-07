e743a3a5142e28e008e6f50e173b3c791c403dda
Charlotte Maples <cgmaples13@gmail.com>
2024-06-07 13:50:33 -0700

    all tests pass :D

--- packages/express-backend/backend.test.js
+++ packages/express-backend/backend.test.js
@@ -1,7 +1,6 @@
 //backend.test.js
 import mongoose from "mongoose";
 import mut from "./user-services";
-import dotenv from "dotenv";
 import userModel from "./schemas/user.js";
 
 beforeAll(() => {
@@ -17,6 +16,19 @@ beforeAll(() => {
   });
 });
 
+test("Testing deleteUserById()", async () => {
+  const email = "eric01@gmail.com";
+  const password = "eheh123"
+  const mockUser = new userModel({ email, password });
+  const savedUser = await mockUser.save();
+  const got = await mut.deleteUserById(savedUser._id);
+  expect(got._id).toBeDefined();
+  expect(got.email).toBe(savedUser.email);
+
+  const deletedUser = await mut.findUserById(savedUser._id);
+  expect(deletedUser).toBeNull();
+});
+
 test("Testing addUser()", async () => {
   const email = "charlieisntbored@gmail.com";
   const password = "calpoly123"
@@ -26,19 +38,19 @@ test("Testing addUser()", async () => {
   expect(got.email).toBe(email);
   expect(got.password).toBeDefined();
 
-  userModel.findByIdAndDelete(got._id);
+  await mut.deleteUserById(got._id);
 });
 
 test("Testing findUserByEmail()", async () => {
     const email = "charlieisbored@gmail.com";
     const password = "calpoly123"
     const mockUser = new userModel({ email, password });
-    await mockUser.save();
+    const savedUser = await mockUser.save();
 
-    const got = await mut.findUserByEmail(email);
-    expect(got[0].email).toBe(email);
+    const got = await mut.findUserByEmail(savedUser.email);
+    expect(got[0].email).toBe(savedUser.email);
 
-    userModel.findByIdAndDelete(got._id);
+    await mut.deleteUserById(got._id);
 });
 
 test("Testing findUserById()", async () => {
@@ -50,17 +62,13 @@ test("Testing findUserById()", async () => {
   const got = await mut.findUserById(savedUser._id);
   expect(got.email).toBe(email);
   expect(got.password).toBe(password);
+
+  await mut.deleteUserById(got._id);
 });
 
 test("Testing getUsers() -- all users", async () => {
   const got = await mut.getUsers();
   expect(got.length).toBeGreaterThanOrEqual(0);
-});
-
-test("Testing deleteUserById()", () => {
-  const expected = "";
-  const got = "";
-  expect(got).toBe(expected);
 });
 
 afterAll(() => {
